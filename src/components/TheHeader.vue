d8d1e247bed1654b613200249dd235a27091b71f
sabithbskumar <65082465+sabithbskumar@users.noreply.github.com>
2024-03-17 21:17:57 +0530

    ✨ add slots in header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -5,9 +5,11 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex justify-between align-middle bg-neutral-700/50 shadow-lg z-10 h-full max-h-16 shrink-0">
+  <div class="flex items-center bg-neutral-700/50 shadow-lg z-10 h-full max-h-16 shrink-0">
+    <slot name="before"></slot>
     <div class="p-4 h-full px-3 text-xl font-semibold">
       <span>{{ heading }}</span>
     </div>
+    <slot name="default"></slot>
   </div>
 </template>
