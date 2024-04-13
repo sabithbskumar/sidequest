d830bea63a16451895d35cbd1d753012d0b6c39a
sabithbskumar <65082465+sabithbskumar@users.noreply.github.com>
2024-04-13 23:39:17 +0530

    💄 minor ui change in transaction list item

--- src/components/Currency/TransactionItem.vue
+++ src/components/Currency/TransactionItem.vue
@@ -30,52 +30,37 @@ const transactionStyle = (type: string) => {
 </script>
 
 <template>
-  <div class="flex justify-between mx-auto w-full max-w-5xl h-14 overflow-clip">
+  <div class="flex gap-2 justify-between mx-auto w-full max-w-5xl h-14 overflow-clip">
     <div
-      class="grow h-full inline-flex items-center gap-2 overflow-hidden rounded bg-neutral-600 bg-opacity-20 hover:bg-opacity-80 shadow-sm"
+      class="grow h-full inline-flex items-center gap-2 overflow-hidden rounded bg-neutral-600 bg-opacity-20 hover:bg-opacity-80 shadow-sm px-2"
       @click.self="$emit('click', transactionId)"
     >
-      <span
-        class="px-2 font-semibold shrink-0 font-mono before:px-1 min-w-[12ch] border-r border-neutral-600 pointer-events-none"
-        :class="transactionStyle(type)"
-      >
-        {{ amount }}
-      </span>
       <span class="px-3 truncate pointer-events-none text-neutral-200 font-medium">{{
         category
       }}</span>
       <span class="px-3 truncate pointer-events-none text-neutral-200 font-medium">{{ note }}</span>
-
-      <Transition name="action-buttons">
-        <div v-if="showActions" class="ml-auto h-full inline-flex">
-          <button
-            type="button"
-            class="outline-none text-neutral-400 hover:text-white focus-visible:ring-inset focus-visible:ring rounded"
-            @click="$emit('edit', transactionId)"
-          >
-            <EditIcon class="size-14 p-3 shrink-0" />
-          </button>
-          <button
-            class="outline-none text-neutral-400 hover:text-red-400 focus-visible:ring-inset focus-visible:ring rounded"
-            type="button"
-            @click="$emit('delete', transactionId)"
-          >
-            <DeleteIcon class="size-14 p-3 shrink-0" />
-          </button>
-        </div>
-      </Transition>
+      <span
+        class="px-2 font-semibold shrink-0 font-mono before:px-1 pointer-events-none ml-auto"
+        :class="transactionStyle(type)"
+      >
+        {{ parseInt(amount).toFixed(2) }}
+      </span>
+    </div>
+    <div v-if="showActions" class="ml-auto h-full inline-flex rounded bg-neutral-600 bg-opacity-20">
+      <button
+        type="button"
+        class="outline-none text-neutral-400 hover:text-white focus-visible:ring-inset focus-visible:ring rounded"
+        @click="$emit('edit', transactionId)"
+      >
+        <EditIcon class="size-14 p-3 shrink-0" />
+      </button>
+      <button
+        class="outline-none text-neutral-400 hover:text-red-400 focus-visible:ring-inset focus-visible:ring rounded"
+        type="button"
+        @click="$emit('delete', transactionId)"
+      >
+        <DeleteIcon class="size-14 p-3 shrink-0" />
+      </button>
     </div>
   </div>
 </template>
-
-<style scoped>
-.action-buttons-enter-active,
-.action-buttons-leave-active {
-  transition: all 0.2s ease;
-}
-
-.action-buttons-enter-from,
-.action-buttons-leave-to {
-  transform: translateX(100%);
-}
-</style>
