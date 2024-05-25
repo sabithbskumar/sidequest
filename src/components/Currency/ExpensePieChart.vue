1adb68a49c7de19efcf99234930ce889fc554e2f
sabithbskumar <65082465+sabithbskumar@users.noreply.github.com>
2024-05-25 19:42:42 +0530

    ✨ add expense pie chart in dashboard

---
+++ src/components/Currency/ExpensePieChart.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { use } from "echarts/core";
+import { SVGRenderer } from "echarts/renderers";
+import { PieChart } from "echarts/charts";
+import { TitleComponent, TooltipComponent, LegendComponent } from "echarts/components";
+import VChart from "vue-echarts";
+import { computed } from "vue";
+import {
+  type ComposeOption,
+  type LegendComponentOption,
+  type PieSeriesOption,
+  type TitleComponentOption,
+  type TooltipComponentOption,
+} from "echarts";
+
+const props = defineProps<{ data: { value: number; name: string }[] }>();
+
+use([SVGRenderer, PieChart, TitleComponent, TooltipComponent, LegendComponent]);
+
+type EChartsOption = ComposeOption<
+  TitleComponentOption | TooltipComponentOption | LegendComponentOption | PieSeriesOption
+>;
+
+const option = computed<EChartsOption>(() => {
+  return {
+    color: ["#fc97af", "#87f7cf", "#f7f494", "#72ccff", "#f7c5a0", "#d4a4eb", "#d2f5a6", "#76f2f2"],
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      top: 5,
+      left: "center",
+      textStyle: {
+        color: "#ddd",
+      },
+    },
+    series: [
+      {
+        name: "Expense",
+        type: "pie",
+        radius: ["40%", "70%"],
+        avoidLabelOverlap: true,
+        padAngle: 2,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: "#fff",
+          borderWidth: 2,
+        },
+        label: {
+          show: true,
+          position: "outer",
+          color: "#ddd",
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 20,
+            fontWeight: "bold",
+          },
+          scaleSize: 10,
+        },
+        labelLine: {
+          show: true,
+        },
+        data: props.data,
+      },
+    ],
+  };
+});
+</script>
+
+<template>
+  <div class="py-5">
+    <VChart class="h-[500px]" :option="option" :autoresize="true" />
+  </div>
+</template>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import { useCurrencyStore } from "@/stores/currency";
+import { useCurrencyStore, type TransactionDetails } from "@/stores/currency";
 import { useQuestStore } from "@/stores/quest";
 import { storeToRefs } from "pinia";
 import CurrencyIcon from "~icons/fluent-emoji-flat/coin";
 import CalendarIcon from "~icons/fluent-emoji-flat/calendar";
 import TheHeader from "@/components/TheHeader.vue";
 import { computed } from "vue";
+import ExpensePieChart from "@/components/Currency/ExpensePieChart.vue";
 
 const currencyStore = useCurrencyStore();
-const { transactionIds, transactionRecords } = storeToRefs(currencyStore);
+const { transactionIds, transactionRecords, categoryRecords } = storeToRefs(currencyStore);
 const { loadData: loadTransactionData } = currencyStore;
 
 const storedTransactionData = localStorage.getItem("currencyStore");
@@ -46,13 +47,34 @@ const storedQuestData = localStorage.getItem("questStore");
 if (storedQuestData) {
   loadQuestData(JSON.parse(storedQuestData));
 }
+
+const expenseTally = computed(() => {
+  const categories = new Map();
+
+  const data: { value: number; name: string }[] = [];
+
+  transactionIds.value.forEach((id) => {
+    const transactionRecord: TransactionDetails = transactionRecords.value[id];
+    if (transactionRecord.type !== "expense") return;
+
+    const { amount, categoryId } = transactionRecord;
+    const category = categories.get(categoryId);
+    categories.set(categoryId, (category || 0) + +amount);
+  });
+
+  categories.forEach((value, key) => {
+    data.push({ value: value.toFixed(2), name: categoryRecords.value[key].name });
+  });
+
+  return data;
+});
 </script>
 
 <template>
   <div class="size-full flex flex-col relative bg-neutral-900">
     <TheHeader heading="Dashboard" />
 
-    <div class="grow bg-neutral-800 overflow-hidden overflow-y-auto p-2">
+    <div class="grow bg-neutral-800 p-2 overflow-y-auto">
       <div class="h-full w-full max-w-5xl mx-auto flex flex-col gap-2">
         <RouterLink :to="{ name: 'quest' }" class="block rounded bg-neutral-700/30">
           <div class="flex items-center gap-3 rounded-t p-2 bg-neutral-700/50">
@@ -63,11 +85,16 @@ if (storedQuestData) {
             <span>{{ questIds.length }} Quests</span>
           </div>
         </RouterLink>
-        <RouterLink :to="{ name: 'currency' }" class="block rounded bg-neutral-700/30">
-          <div class="flex items-center gap-3 rounded-t p-2 bg-neutral-700/50">
-            <CurrencyIcon class="size-8" />
-            <span>Currency</span>
-          </div>
+        <div class="block rounded bg-neutral-700/30">
+          <RouterLink
+            :to="{ name: 'currency' }"
+            class="block rounded overflow-hidden bg-neutral-700/30"
+          >
+            <div class="flex items-center gap-3 rounded-t p-2 bg-neutral-700/50">
+              <CurrencyIcon class="size-8" />
+              <span>Currency</span>
+            </div>
+          </RouterLink>
           <div class="p-3 flex justify-between">
             <span class="text-green-500">Income: {{ financeTally.income.toFixed(2) }}</span>
             <span class="text-red-400">Expense: {{ financeTally.expense.toFixed(2) }}</span>
@@ -75,7 +102,8 @@ if (storedQuestData) {
               >Balance: {{ financeTally.balance.toFixed(2) }}</span
             >
           </div>
-        </RouterLink>
+          <ExpensePieChart :data="expenseTally" />
+        </div>
       </div>
     </div>
   </div>
